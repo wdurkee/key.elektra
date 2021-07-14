f2517e56bb9728ce42a3e994bb9be5a86997888b
Will Durkee <william.w.durkee@gmail.com>
2021-07-14 14:12:54 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className="container">
+      <video  preload="yes" playsInline autoPlay loop muted className="video" >
+        <source src="https://i.imgur.com/xnxwypk.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+        <div className='content'>
+          <div className='sub-content'>
+            <div className='button-container'>
+              <a className='portal-button' href='google.com' target="_blank" rel="noreferrer">
+                CHI TICKET
+              </a>
+              <a className='portal-button psi' href='google.com' target="_blank" rel="noreferrer">
+                PSI TICKET
+              </a>
+              <a className='portal-button phi' href='google.com' target="_blank" rel="noreferrer">
+                PHI TICKET
+              </a>
+            </div>
+            </div>  
+          <div className="powerBottom">
+            {/* <div className="logo">
+              <a href="https://songcamp.mirror.xyz/mkb-m9GWUCLByETAvCigJ0JIpz4XH8bwNDBtUJBPeFc" target="_blank" rel="noreferrer">
+                <ElektraSatellite />
+              </a>
+            </div> */}
+              Powered by <a href="https://twitter.com/songcamp_" target="_blank" rel="noreferrer">Songcamp</a>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
 export default App;
